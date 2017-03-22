269727cd1eb746536a058e2ea81aebbfb563951a
attila <attila@stalphonsos.com>
2017-03-22 14:09:12 -0600

    Add dot/sample fu, mung things slightly, still getting over the shock that I wrote NO TESTS

--- lib/Flail/App.pm
+++ lib/Flail/App.pm
@@ -1,16 +1,23 @@
+#! perl
+# -*- mode:perl;tab-width:8;perl-indent-level:8;indent-tabs-mode:t -*-
+
 =pod
 
 =head1 NAME
 
-Flail::App - Flail application base class
+Flail::App - top-level application object for flail
 
 =head1 SYNOPSIS
 
-  # c.f. App::Cmd
+  Flail::App->run;
 
 =head1 DESCRIPTION
 
-The base class for the Flail app.  See the App::Cmd documentation.
+Blah.
+
+=head1 LICENSE
+
+ISC/BSD; see LICENSE file in source distribution.
 
 =cut
 
@@ -19,47 +26,8 @@ use strict;
 use warnings;
 use App::Cmd::Setup -app;
 
+sub allow_any_unambiguous_abbrev { 1; }
+
 1;
 
 __END__
-
-=pod
-
-=head1 SEE ALSO
-
-We live in splendid isolation.
-
-=head1 AUTHOR
-
-  attila <attila@stalphonsos.com>
-
-=head1 LICENSE
-
-Copyright (C) 2015 by attila <attila@stalphonsos.com>
-
-Permission to use, copy, modify, and distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
-WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
-AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
-DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
-PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
-TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
-PERFORMANCE OF THIS SOFTWARE.
-
-=cut
-
-##
-# Local variables:
-# mode: perl
-# tab-width: 4
-# perl-indent-level: 4
-# cperl-indent-level: 4
-# cperl-continued-statement-offset: 4
-# indent-tabs-mode: nil
-# comment-column: 40
-# End:
-##
